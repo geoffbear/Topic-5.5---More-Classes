b3e0878d9415511c804db9383352fcda5e02fefd
Geoffrey Dolbear <339470494@GoTVDSB.ca>
2024-03-20 12:48:53 -0400

    changes

--- Program.cs
+++ Program.cs
@@ -4,6 +4,28 @@
     {
         static void Main(string[] args)
         {
+            int balance = 100;
+            int bet;
+            Console.WriteLine("You have $100, how much would you like to bet?");
+            bet = Convert.ToInt32(Console.ReadLine());
+            if (bet > 100)
+            {
+                bet = 100;
+            }
+            Console.WriteLine("You have chosen to bet $" + bet );
+            Console.WriteLine("Would you like to bet on doubles, 1/6 odds, winning x2 your bet back\nWould you like to bet on non doubles, 5/6 odds, winning half your bet back\n");
+
+
+
+
+
+
+
+
+
+
+
+
             Die die1;
             Die die2;
             die1 = new Die();
